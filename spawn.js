e56714d54d4f8cc4a6f4f46e6d518ea0ecbd1e38
voxelbugged <47631415+voxelbugged@users.noreply.github.com>
2023-11-05 16:46:42 +0100

    Added more upgrades

--- spawn.js
+++ spawn.js
@@ -4,7 +4,6 @@ var changed = false;
 function cool() {
     document.documentElement.requestPointerLock();
     myWindow = window.open("groove.html", "", "menubar=no, status=no, toolbar=no, resizable=no, width=300, height=175, titlebar=no, alwaysRaised=yes");
-    document.documentElement.requestFullscreen();
     if (document.getElementById("main")) {
         if (!played) {
             var audio = new Audio('audio_midloud.mp3');
@@ -14,28 +13,39 @@ function cool() {
         document.getElementById("main").style.cursor = 'none';
     }
     if (!changed) {
-        document.getElementById("game").innerHTML = "<h1>WE CALL THIS A DIFFICULTY TWEAK!</h1> <hr> <img src='cloaker-payday2.gif'>"
+        if(document.getElementById("game"))
+        {
+            document.getElementById("game").innerHTML = "<h1>WE CALL THIS A DIFFICULTY TWEAK!</h1> <hr> <img src='cloaker-payday2.gif'>"
+        }
         changed = true;
     }
 }
 
 window.onclick = function (event) {
     cool();
+    return false;
 }
 
 window.oncontextmenu = function (event) {
     cool();
-    false;
+    return false;
 }
 
 window.onscroll = function (event) {
     cool();
+    return false;
 }
 
 window.onkeydown = function(event) {
     cool();
+    return false;
+}
+
+window.onkeydown = function(event) {
+    cool();
+    return false;
 }
 
 window.onbeforeunload = function(event) {
-    "no xd"
+    return "nope";
 }
