045fa9223fb6ad2866054498a723b18360474011
voxelbugged <47631415+voxelbugged@users.noreply.github.com>
2025-09-17 15:05:22 +0200

    i did my math wrong

--- spawn.js
+++ spawn.js
@@ -9,7 +9,7 @@ function cool() {
     if (document.getElementById("main")) {
         if (!played) {
             document.title = "I GOT YOUR DLC RIGHT HERE!";
-            setInterval(fuckbackground, 521);
+            setInterval(fuckbackground, 480);
             document.getElementById("game").innerHTML = "<h1>WE CALL THIS A DIFFICULTY TWEAK!</h1> <hr> <marquee scrollamount=125 behavior='alternate'><img src='cloaker-payday2.gif'></marquee>";
             var audio = new Audio('audio_midloud.mp3');
             audio.play();
